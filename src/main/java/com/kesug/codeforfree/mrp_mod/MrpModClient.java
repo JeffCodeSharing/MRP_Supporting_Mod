eebe865591a7191a9cfe2b3e36200286e4d923e0
AIR <3545210791@qq.com>
2023-12-24 09:17:38 +0800

    update

---
+++ src/main/java/com/kesug/codeforfree/mrp_mod/MrpModClient.java
@@ -0,0 +1,11 @@
+package com.kesug.codeforfree.mrp_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MrpModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
