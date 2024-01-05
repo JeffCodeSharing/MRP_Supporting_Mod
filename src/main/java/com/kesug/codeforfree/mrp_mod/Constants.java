9811ab563866856dfd9c2088c00879773fb12a7a
AIR <3545210791@qq.com>
2024-01-05 21:07:23 +0800

    update

---
+++ src/main/java/com/kesug/codeforfree/mrp_mod/Constants.java
@@ -0,0 +1,5 @@
+package com.kesug.codeforfree.mrp_mod;
+
+public class Constants {
+    public final static String MOD_ID = "mrp_mod";
+}
--- src/main/java/com/kesug/codeforfree/mrp_mod/MrpModClient.java
+++ src/main/java/com/kesug/codeforfree/mrp_mod/MrpModClient.java
@@ -1,11 +1,14 @@
 package com.kesug.codeforfree.mrp_mod;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MrpModClient implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
     @Override
     public void onInitializeClient() {
-
+        LOGGER.info("Hello Dear Player.");
     }
 }
